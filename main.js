68304f5cf85678fc9e7af47523565a0c6727982a
moududulhaque <moududulhaque@gmail.com>
2024-12-29 07:08:29 +0600

    1st Class is Done

--- main.js
+++ main.js
@@ -15,3 +15,68 @@ if (3 > 5) {
 } else {
     console.log("object");
 }
+
+let day = 'Saturday';
+
+switch (day) {
+    case 'Saturday':
+        console.log("THis is saturday");
+        break;
+
+    case 'Sunday is':
+        console.log("THis is Sunday");
+        break;
+
+    case 'Monday is':
+        console.log("THis is monday");
+        break;
+
+    default:
+        console.log("This is default day");
+}
+
+let month = '12';
+
+switch (month) {
+    case '1':
+        console.log("January");
+        break;
+    case '2':
+        console.log("January");
+        break;
+    case '3':
+        console.log("January");
+        break;
+    case '4':
+        console.log("FEbruay");
+        break;
+    case '5':
+        console.log("March");
+        break;
+    case '6':
+        console.log("April");
+        break;
+    case '7':
+        console.log("May");
+        break;
+    case '8':
+        console.log("June");
+        break;
+    case '9':
+        console.log("July");
+        break;
+    case '10':
+        console.log("August");
+        break;
+    case '11':
+        console.log("September");
+        break;
+    case '12':
+        console.log("October");
+        break;
+
+    default:
+        console.log("Nothing");
+
+}
+
