1c2a1ccdea1e20322ee4e7dbb2f991e2df6376c3
moududulhaque <moududulhaque@gmail.com>
2025-01-19 20:00:19 +0600

    COmments Done

--- practices/js/first.js
+++ practices/js/first.js
@@ -1,6 +1,29 @@
 //Javascripts Single Line Comment
+// এটি একটি user এর নাম 
+let userName = "Rasel Ahmed"
 
 /*
 JavaScripts Double Line Comment
 This is very important to understand code
 */
+/* 
+এটি একটি স্টুডেন্ট এর তথ্য:
+নাম: Rasel
+রোল: ১০১ 
+ক্লাস: ১০ম
+সেকশন: এ
+*/
+let studentName = "Rasel";
+let roll = 101;
+
+/*
+যোগাযোগের তথ্য:
+ফোন: ০১৭১১-০০০০০০
+ইমেইল: rasel@email.com  
+ঠিকানা: ঢাকা, বাংলাদেশ
+*/
+let contact = {
+    phone: "01711-000000",
+    email: "rasel@email.com",
+    address: "Dhaka, Bangladesh"
+};
